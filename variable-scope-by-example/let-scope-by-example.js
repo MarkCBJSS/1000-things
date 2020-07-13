7dfe87001b7e6c1ade82f0237c533aabdcbd3506
Mark Crowther <mark.crowther@bjss.com>
2020-07-13 15:19:57 +0100

    Adde variable scope examples

--- variable-scope-by-example/let-scope-by-example.js
+++ variable-scope-by-example/let-scope-by-example.js
@@ -1,11 +1,11 @@
-var aVariableOutsideACodeBlock = 10;
+let aVariableOutsideACodeBlock = 10;
 
     if (aVariableOutsideACodeBlock == 10) {
-        //var aVariableOutsideACodeBlock = 20;
-        var aVariableINSIDE = 30;
+        let aVariableINSIDE = 20;
         console.log("aVariableOutsideACodeBlock called from INSIDE the code block is : " + aVariableOutsideACodeBlock);
         console.log("aVariableINSIDE called inside and set inside is: " + aVariableINSIDE);
     }
 // The below works if we use var, change the var keywords to let and it will fail
-// This demonstrates the better scope control of let 
+// This demonstrates the better scope control of let
+console.log("## We now call a let set inside a block, but from the outside, so should see an error");
 console.log("aVariableINSIDE called from OUTSIDE a code block, but set INSIDE that code block is: " + aVariableINSIDE);
